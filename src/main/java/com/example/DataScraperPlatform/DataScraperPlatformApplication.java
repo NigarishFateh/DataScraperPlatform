f064dfff1931cffce93517bc200fabb045f1f35b
NigarishFateh <nagarashfateh@gmail.com>
2026-07-20 13:14:53 +0500

    project initialized

---
+++ src/main/java/com/example/DataScraperPlatform/DataScraperPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.example.DataScraperPlatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataScraperPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataScraperPlatformApplication.class, args);
+	}
+
+}
